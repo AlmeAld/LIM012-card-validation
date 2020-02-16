12aeb239cc667aca97c75f81f7088e26900e4f86
AlmeAld <almenaldana@gmail.com>
2020-02-15 21:49:48 -0500

    subiendo cambios a javaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const validar = document.getElementById('validar')
+
+validar.addEventListener('click',() =>{
+const cardNumber = parseInt(document.getElementById("cardNumber").value)
+const validated = document.getElementById(validated)
+const cardData = document.getElementById(cardData)
+cardData.style.display='none'
+validated.style.display='block'
+
+});
--- src/validator.js
+++ src/validator.js
@@ -3,3 +3,19 @@ const validator = {
 };
 
 export default validator;
+const numTarjeta = '4556364607935616';
+//let numero = prompt('¿Cual es el número de tu tarjeta?')
+let arr = numTarjeta.split('').reverse()
+let suma = 0;
+let n = 0;
+for(let i=0; i<arr.length; i++){
+  if(i%2 === 0){
+    n= parseInt(arr[i])*2;
+    if(n>=10){
+      n= (n-10)+1
+    } 
+  } else{
+    n= parseInt(arr[i])
+  }
+  suma = suma + n
+}
