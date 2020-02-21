3b51f059660f2ca2cab1de725ee2524d04ca399f
AlmeAld <almenaldana@gmail.com>
2020-02-21 12:04:53 -0500

    validador.js terminado

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import validator from './validator.js';
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 3ccbffc9eb11d7f9d3544e86f1c1eb38fd4842f9
+//console.log(validator)
+//console.log(validator.isValid)
+//console.log(validator.isValid('4556364607935616'))
 const validar = document.getElementById('validar')
-
 validar.addEventListener('click',() =>{
-const cardNumber = document.getElementById('cardNumber').value
-const cardData = document.getElementById('validar')
-validar.addEventListener('click',() =>{
-
-})
+    const cardNumber = document.getElementById('cardNumber').value 
+    console.log(cardNumber)
+    console.log(validator.isValid(cardNumber))
 cardData.style.display='none'
-validated.style.display='none'
+validated.style.display='block'
 
 });
+
--- src/validator.js
+++ src/validator.js
@@ -1,54 +1,41 @@
 const validator = {
   // ...
-  //isValid:(numTarjeta)=>{
-  };
-    const numTarjeta = '455636460793561 6'
-    let arr = numTarjeta.split('').reverse()
+  isValid:(creditCardNumber)=>{
+
+    let arr = creditCardNumber.split('').reverse()
     let suma = 0;
-    let n = 0;
-    for(let i=0; i<arr.length; i++){
-      if(i%2 === 0){
-        n= parseInt(arr[i])*2;
-        if(n>=10){
-          n= (n-10)+1
-        } 
-      } else{
-        n= parseInt(arr[i])
+  let n = 0;
+  for(let i=0; i<arr.length; i++){
+    if(i%2 === 0){
+      n= parseInt(arr[i])*2;
+      if(n>=10){
+        n= (n-10)+1
       }
-      suma = suma + n
-    }
-  
-    if(suma % 10 === 0){
-       console.log('tarjeta valida')
-     }else{
-      console.log('tarjeta invalida')
-      }
-
- // maskify:(numTarjeta)=>{
-    //for(let i=0; i<; i++){
-
-      
-
-    //}
- //}
-
- /*const numTarjeta = '4556364607935616';
-let arr = numTarjeta.split('')
-let string = '';
-
-for(i=0; i < arr.length; i++){
-  if(i < 12){
-    arr[i] = '#'
-    //string = string + arr[i]
-  }else{
-   arr[i]
-   string = string + arr[i]
+  } else{
+    n= parseInt(arr[i])
   }
-} */
+  suma = suma + n
+  }
+  return suma % 10 === 0 ? true : false
+  },
 
+     
 
-
-
-
+ maskify : (creditCardNumber)=>{
+    let arr = creditCardNumber.split('')
+    let string = '';
+    
+    for(i=0; i < arr.length; i++){
+      if(i < 12){
+        arr[i] = '#'
+        string = string + arr[i]
+      }else{
+        string = string + arr[i]
+      }
+    } return string
+  }
+  
+};
+  
 export default validator;
 
