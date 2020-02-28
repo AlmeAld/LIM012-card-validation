edf8bfb83cc0ddc2b0e6d6045deda9c89aba7623
almeald <almenaldana@gmail.com>
2020-02-28 00:33:07 -0500

    css grid

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ const btnValidar = document.getElementById('btnValidar')
 
 const btnVolver = document.getElementById('btnVolver')
 const numValidado = document.getElementById('numValidado')
+const contenedor2= document.getElementById('contenedor2')
+const contenedor1= document.getElementById('contenedor1')
+//debugger
 
 
 btnValidar.addEventListener('click', () =>{
-  
+    
     const cardNumber = document.getElementById('cardNumber').value 
-         
-const isValid =  validator.isValid(cardNumber)
-
+    
+    const isValid =  validator.isValid(cardNumber)
+    
     if(isValid === true){
         numValidado.textContent = validator.maskify(cardNumber) + 'TARJETA VALIDA'        
     }else{        
         numValidado.textContent = validator.maskify(cardNumber) + 'TARJETA INVALIDA'        
     }
-
+    contenedor2.style.display='block'
+    contenedor1.style.display='none'
 }); 
 
 
@@ -25,7 +29,3 @@ btnVolver.addEventListener('click', () => {
 
     window.location.reload(false)
 });
-
-//console.log(validator)
-//console.log(validator.isValid)
-//console.log(validator.isValid('4556364607935616'))
